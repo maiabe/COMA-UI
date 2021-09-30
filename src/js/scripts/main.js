ebd5b6405e16b8bfc65f3dcaa3998a1c37eb9b83
James <jhutch42@hawaii.edu>
2021-09-30 12:59:05 -1000

    many changes

--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -18,10 +18,12 @@ const blueMunsell = '#2C8C99';
 const turquoise = '#42D9C8';
 const darkLiver = '#52414C';
 const ebony = '#596157';
-const middleGreen = '#5B8c5a';
+const middleGreen = '#3f743e';
+
+const darkRed = '#ac232f';
 // Viz Pallete
 const darkGreen = '#003f5c';
-const blue = '#2f4b7c';
+const blue = '#1a3c77';
 const darkPurple = '#665191';
 const lightPurple = '#a05195';
 const pink = '#d45087';
@@ -29,13 +31,13 @@ const salmon = '#f95d61';
 const darkOrange = '#ff7c42';
 const lightOrange = 'ffa600';
 
-const sourceColor =middleGreen ;
-const processorColor = darkPurple;
+const sourceColor = middleGreen;
+const processorColor = darkRed;
 const outputColor = blue;
 // Global Manager Instance
 let GM;
 
-function init () {
+function init() {
     GM = new GlobalManager();
     GM.startEnvironment();
 }
